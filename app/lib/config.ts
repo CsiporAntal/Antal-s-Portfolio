eb254ff6534daac68dbfc959afde49eeb2de0f8e
toni <tonicsipor@gmail.com>
2025-07-16 04:35:26 +0300

    Enchanted Profile image.

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -2,7 +2,7 @@ export const metaData = {
   baseUrl: "https://antal-s-portfolio.vercel.app",
   title: "Antal's Portfolio",
   name: "Antal",
-  ogImage: "/opengraph-image.png",
+  ogImage: "/profile.png",
   description:
       "Personal portfolio of Csipor Antal — PLC programmer and web developer based in Romania.",
 };
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <section>
         <a href={socialLinks.github} target="_blank">
           <Image
-              src="/profile.jpg"
+              src="/profile.png"
               alt="Profile photo"
               className="rounded-full bg-gray-100 block mt-6 lg:mt-10 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale-[50%] hover:grayscale-0 transition duration-300"
               unoptimized
